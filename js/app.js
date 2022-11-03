8697f5291e743387b94441760947c9d6e2a19a9e
Birkan Yılmaz <59147411+birkaany@users.noreply.github.com>
2022-11-04 01:02:57 +0300

    fix: add flipping card animations

--- js/app.js
+++ js/app.js
@@ -3,13 +3,14 @@ const wallet = document.querySelector("#wallet");
 const openedCards = document.querySelector("#openedCards");
 const bet = document.querySelector("#bet");
 const cardsTotal = document.querySelector("#cardsTotal");
+let cardsBlock = document.getElementById("cards");
+const message = document.getElementById("message");
 
 let playerName = (document.querySelector("#playerName").textContent = window.prompt("Your name"));
 let budget = Number(window.prompt("How much money in your wallet?"));
 
 let isGameOver = false;
 let isRoundOver = false;
-
 let betAmount = budget / 10;
 let sumCards;
 let firstCards;
@@ -25,9 +26,46 @@ newGame = () => {
   wallet.textContent = `$${budget}`;
   bet.textContent = `$${betAmount}`;
   cardsTotal.textContent = sumCards;
+  message.textContent = "";
+  firstCards.forEach((cards) => {
+    var div = document.createElement("div");
+    div.setAttribute("class", "flip-card");
+    div.innerHTML = `
+      <div class="flip-card-inner">
+        <div class="flip-card-front">
+          <img src="./images/card-back.png" />
+        </div>
+        <div class="flip-card-back">
+          <img src="./images/card${cards}.png" />
+        </div>
+      </div>`;
+    cardsBlock.appendChild(div);
+  });
 };
 
 newGame();
+
+cleanCards = () => {
+  let activeCards = document.querySelectorAll(".flip-card");
+  activeCards.forEach((activeCards) => {
+    activeCards.remove();
+  });
+};
+
+showNextCard = (cardNumber) => {
+  var div = document.createElement("div");
+  div.setAttribute("class", "flip-card");
+  div.innerHTML = `
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="./images/card-back.png" />
+    </div>
+    <div class="flip-card-back">
+      <img src="./images/card${cardNumber}.png" />
+    </div>
+  </div>`;
+  cardsBlock.appendChild(div);
+};
 
 pickNextCard = () => {
   if (budget >= betAmount) {
@@ -37,15 +75,19 @@ pickNextCard = () => {
     cardsTotal.textContent = sumCards;
 
     if (sumCards < 21) {
+      showNextCard(nextCard);
     } else if (sumCards == 21) {
-      console.log(`"Won!"`);
       budget += betAmount;
       wallet.textContent = `$${budget}`;
+      isRoundOver = true;
+      cleanCards();
+      message.textContent = `You won $${betAmount}`;
     } else {
-      console.log(`Total: ${sumCards} | You lost`);
       isRoundOver = true;
       budget -= betAmount;
       wallet.textContent = `$${budget}`;
+      cleanCards();
+      message.textContent = `You lost $${betAmount}`;
     }
   } else {
     isGameOver = true;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.{html,js}"],
+  safelist: ["flip-card-action"],
   theme: {
     extend: {
       colors: {
@@ -8,6 +9,9 @@ module.exports = {
         "th-yellow-50": "#f2e8cf",
         "th-gray-200": "#434343",
         "th-green-700": "#386641",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
       },
       fontFamily: {
         primaryFont: ["Roboto", "sans-serif"],
